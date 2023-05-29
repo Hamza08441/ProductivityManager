91def37b6f07b1e3135d2e1601fcc7cf6b94d04a
70dakhaled <mahmoud2008975@miuegypt.edu.eg>
2023-05-29 21:49:19 +0300

    find emplyee&project

--- webapi/services/EmployeeService/IEmployeeService.cs
+++ webapi/services/EmployeeService/IEmployeeService.cs
@@ -7,7 +7,7 @@ namespace webapi.services.EmployeeService
     {
         Employees addEmployee(EmployeesVM employeesVM);
         EmployeesVM checkEmail(EmployeesVM employeesVM);
-        // EmployeesVM viewBYID(EmployeesVM employeesVM);
-        public Employees FindEmployeeById(Guid employeeId);
+       
+         Employees FindEmployeeById(Guid employeeId);
     }
 }
